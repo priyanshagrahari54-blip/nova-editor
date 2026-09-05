ade40905b7b123904d3cb69ad90ac1a8ba1f8038
priyanshagrahari54-blip <priyanshagrahari54@gmail.com>
2026-09-05 16:46:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11726473
+    "domain": "5gvci.com",
+    "zoneId": 11726456
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
